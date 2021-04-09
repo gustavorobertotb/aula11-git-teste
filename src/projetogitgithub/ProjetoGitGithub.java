5d6f800193c46f290384e135503a2a0145f84ab0
gustavorobertotb <gustavo.souza97@outlook.com>
2021-04-09 11:51:36 -0300

    Primeiro COMMIT

---
+++ src/projetogitgithub/ProjetoGitGithub.java
@@ -0,0 +1,16 @@
+
+package projetogitgithub;
+
+import java.util.Scanner;
+
+
+public class ProjetoGitGithub {
+
+
+    public static void main(String[] args) {
+        System.out.println("BEM VINDO");
+        
+        Scanner s = new Scanner(System.in);
+    }
+    
+}
